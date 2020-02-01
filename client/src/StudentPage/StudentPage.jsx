e50d1ed5f9ca0eb7efe8348ef12ee20d7b11306d
pravin_arzooo <pravin@homeyantra.com>
2020-02-01 12:57:31 +0530

    WIP for dropdown for cclass

--- client/src/StudentPage/StudentPage.jsx
+++ client/src/StudentPage/StudentPage.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import axios from "axios";
 import "./StudentPage.scss";
 import Input from '@material-ui/core/Input';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import { makeStyles } from '@material-ui/core/styles';
 
 export class StudentPage extends React.Component {
   constructor(props) {
@@ -11,6 +16,11 @@ export class StudentPage extends React.Component {
       lastName: null,
       email: null,
       password: null,
+      classOpen:false,
+      secOpen:false,
+      rollNo: null,
+      class: null,
+      section: null,
       formErrors: {
         firstName: "",
         lastName: "",
@@ -18,19 +28,51 @@ export class StudentPage extends React.Component {
         password: ""
       }
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleOpen(e) {
+    const {name} = e.target;
+    if(name == 'section'){
+      this.setState({secOpen: true})
+    }
+    else{
+      this.setState({classOpen:true})
+    } 
+  }
+
+  handleClose(e) {
+    const {name} = e.target;
+    if(name == 'section'){
+      this.setState({secOpen: false})
+    }
+    else{
+      this.setState({classOpen:false})
+    } 
+  }
+  
 
 
 
 
 render() {
     const { formErrors } = this.state;
+   
 
     return (
       <div className="wrapper">
         <div className="form-wrapper">
           <h1>Add Student</h1>
           <form onSubmit={this.handleSubmit} noValidate>
+          <div className = "name">
             <div className="firstName">
               <Input
                 className={formErrors.firstName.length > 0 ? "error" : null}
@@ -66,15 +108,62 @@ render() {
                 <span className="errorMessage">{formErrors.lastName}</span>
               )}
             </div>
-            <div>
+            </div>
+            <div className = "class">
             <Input 
               placeholder = 'Roll No'
               required = "true"
-              name = "RollNo"
+              name = "rollNo"
               type = "number"
               onChange = {this.handleChange}
               />
+
+              <FormControl className="formControl">
+             <InputLabel id="demo-controlled-open-select-label">Age</InputLabel>
+             <Select
+                labelId="demo-controlled-open-select-label"
+                id="demo-controlled-open-select"
+                open={this.state.Classopen}
+                name = "class"
+                onClose={this.handleClose}
+                onOpen={this.handleOpen}
+                value={this.state.class}
+                onChange={this.handleChange}
+              >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+      </FormControl>
+
+        <FormControl className="formControl">
+             <InputLabel id="demo-controlled-open-select-label">Age</InputLabel>
+             <Select
+                labelId="demo-controlled-open-select-label"
+                id="demo-controlled-open-select"
+                name = "section"
+                open={this.state.SecOpen}
+                onClose={this.handleClose}
+                onOpen={this.handleOpen}
+                value={this.state.section}
+                onChange={this.handleChange}
+              >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+      </FormControl>
+           
               </div>
+              
+             
+
 
            
             <div className="createAccount">
