702ecbedf246d79d4f2bfceb828d222208179fd0
pravin_arzooo <pravin@homeyantra.com>
2020-02-12 14:19:44 +0530

    Pass the fee detail from backend

--- backend/student/index.js
+++ backend/student/index.js
@@ -37,19 +37,29 @@ async function getStudentDetail(req, res, params) {
 	if (validatorResp.error) return validatorResp;
 	let {schoolId} = req.session
 	let {studentId } = req.query;
-	let statement = `select s.name, s.roll_no, s.id, cs.section
-	                 from students s
-	                 left join class_section cs on s.class_section_id = cs.id
-	                 where s.name like '${searchPhrase}%' and s.school_id = ?
-	                 and cs.class = ?`;
-	let values = [schoolId, studentClass];
-	let addStudentResp = await dbUtils.sqlExecutorAsync(req, res, statement, values);
-	
-	return addStudentResp;
+	let statement = `select q2.student_id, q2.amount, q2.created_on, 
+	                 q2.updated_on, q2.remarks,
+                     q1.total_due
+					 from
+					 (
+					 select student_id, SUM(amount) as total_due
+                     from payments
+					 where student_id = ? group by student_id 
+					 ) q1,
+					 (
+					 select student_id, amount, remarks, created_on, updated_on
+					 from payments where student_id = ?
+					 ) q2
+					 where q1.student_id = q2.student_id`;
+	                 
+	let values = [studentId, studentId];
+	let detailResp = await dbUtils.sqlExecutorAsync(req, res, statement, values);
+	return detailResp;
 }
 
 
 module.exports = {
 	addStudent,
-	searchStudent
+	searchStudent,
+	getStudentDetail
 };
