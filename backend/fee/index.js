2ca099a5c6c849fb03f55210681dcc238a34b095
pravin_arzooo <pravin@homeyantra.com>
2020-02-19 18:49:34 +0530

    Check if user is loggedin and ensure the student is registered under his school

--- backend/fee/index.js
+++ backend/fee/index.js
@@ -5,7 +5,16 @@ async function payFee(req, res, params) {
 	const validatorResp = feeValidator.payFeeValidator(params);
 	if (validatorResp.error) return validatorResp;
 	let { studentId, amount, mop } = req.body;
-	console.log("req.b++=", req.body)
+	let {schoolId} = req.session
+	let schoolStatement = `select id from students where school_id = ? and id = ?`;
+	let schoolValues = [schoolId, studentId]
+	let schoolResp = await dbUtils.sqlExecutorAsync(req, res, schoolStatement, schoolValues);
+	if(schoolResp.status === 'error') return schoolResp
+    if(schoolResp.data.length == 0) {
+    	schoolResp.status = 'error'
+    	schoolResp.msg  = "Student is not registered under your school"
+    	return schoolResp
+    }
 	let statement = `insert into payments (student_id, amount, remarks) values (?, ?, ?)`;
 	let values = [studentId, amount, mop];
 	let feePaymentResp = await dbUtils.sqlExecutorAsync(req, res, statement, values);
--- backend/fee/router.js
+++ backend/fee/router.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const lodash = require("lodash");
 const feeFactory = require("./index.js");
+const { isAuthenticated } = require(`${__base}/user/helper`);
 
-router.post("/payFee", async (req, res) => {
+
+router.post("/payFee", isAuthenticated,  async (req, res) => {
 	const params = lodash.pick(req.body, ["studentId", "amount"]);
 	const feePaymentResp = await feeFactory.payFee(req, res, params);
 	res.send(feePaymentResp);
