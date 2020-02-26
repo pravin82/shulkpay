13028ab548e2af1ad2bb2d6e7aadef1038ab1053
pravin_arzooo <pravin@homeyantra.com>
2020-02-26 06:12:33 +0530

    DePLOYING react

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,12 @@ const PORT = process.env.npm_package_config_port || 4000;
 const API_PORT = 3001;
 const apiRouter = require(`${__base}/routes/router`);
 
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
+
+
 const corsOptions = {
     origin: 'http://localhost:3000',
     credentials: true,
