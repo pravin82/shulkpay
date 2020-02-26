65997711bba70007ac6ca69e6ceefc0e1beaa9a5
pravin_arzooo <pravin@homeyantra.com>
2020-02-26 06:01:22 +0530

    Change the AWS IP

--- client/src/constant.js
+++ client/src/constant.js
@@ -4,10 +4,10 @@ if (process.env.NODE_ENV === "development") {
 	baseUrl = "http://shulkpay.test:4000";
 } else if (process.env.NODE_ENV === "production") {
 	//baseUrl = "https://shulkpay.com/";
-	baseUrl = "http://ec2-54-167-129-165.compute-1.amazonaws.com:4000"
+	baseUrl = "http://ec2-3-83-101-88.compute-1.amazonaws.com:4000"
 } else {
 	//baseUrl = "https://test.shulkpay.com";
-	baseUrl = "http://ec2-54-167-129-165.compute-1.amazonaws.com:4000"
+	baseUrl = "http://ec2-3-83-101-88.compute-1.amazonaws.com:4000"
 
 }
 
