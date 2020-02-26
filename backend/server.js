72daf2adf21f1b4144c7c9b9872286029c205a28
pravin_arzooo <pravin@homeyantra.com>
2020-02-26 22:15:47 +0530

    Enable cache

--- backend/server.js
+++ backend/server.js
@@ -33,9 +33,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(session());
 if(isProd){
- // app.enable('view cache')
-  app.disable("view cache");
-  app.disable('etag')
+  app.enable('view cache')
+  //app.disable("view cache");
+  //app.disable('etag')
   app.use(vhost('ec2-3-83-101-88.compute-1.amazonaws.com', apiRouter))
 }
 else {
