157c3faf36610f5469729944aa7288f8b7c34c30
pravin_arzooo <pravin@homeyantra.com>
2020-03-02 11:47:56 +0530

    Add cookie-session

--- backend/database/session.js
+++ backend/database/session.js
@@ -1,24 +1,28 @@
 "use strict";
 
-const session = require("express-session");
-const uuid = require("uuid/v4");
-const AWS = require("aws-sdk");
+const session = require("cookie-session");
+//const uuid = require("uuid/v4");
+//const AWS = require("aws-sdk");
+/*
 const DynamoDBStore = require("connect-dynamodb")({
   session: session
 });
+*/
 
 
-AWS.config.region = "us-east-1";
-
+//AWS.config.region = "us-east-1";
+/*
 const dynamoDBConfig = {
   table: "shulkpay-session",
   client: new AWS.DynamoDB(),
   AWSRegion: "us-east-1",
   reapInterval: 3600 * 1000
 };
+*/
 
 
 const globalSessionConfig = {
+  /*
   cookie: {
     httpOnly: false,
     domain: ".shulkpay.test",
@@ -29,6 +33,7 @@ const globalSessionConfig = {
   genid: function(req) {
     return uuid();
   },
+  */
   name: "sessionId",
   proxy: true,
   secret: "pravin",
@@ -38,7 +43,7 @@ const globalSessionConfig = {
 
 module.exports = function() {
   let envConfig = {
-    store: session.MemoryStore()
+    //store: session.MemoryStore()
   };
  
 
